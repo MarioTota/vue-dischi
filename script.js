85ec0422119c9bf249ed0cc1b013552de6af0ee2
Mario <mario.tota.mt@gmail.com>
2020-12-20 00:53:35 +0100

    commit

--- script.js
+++ script.js
@@ -2,18 +2,30 @@ var app = new Vue({
   el: '#root',
   data: {
     cds: [],
-    selezione: ""
+    selectedValue:'',
+    genres:[]
 	},
   methods: {
 
   },
   mounted:
     function () {
+      var self = this;
       axios
         .get('https://flynn.boolean.careers/exercises/api/array/music')
-        .then((result) => {
-          this.cds = result.data.response;
-          console.log(this.cds);
-        });
+        .then(function(result)  {
+          self.cds = result.data.response;
+
+          self.cds.sort( function(cd1, cd2) {
+            return parseInt(cd1.year) - parseInt(cd2.year);
+          });
+
+          self.cds.forEach(
+            (element) => {
+              if(!self.genres.includes(element.genre)) {
+                self.genres.push(element.genre)
+              }
+          })
+      });
     }
 });
